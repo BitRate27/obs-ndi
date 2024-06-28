cbe066aa3fc6987d7ee53bc1017ecd4b9d89fdfd
BitRate27 <bgillocksbc@yahoo.com>
2024-06-28 14:35:42 -0500

    Run controller thread in QThread and invokeMethod on dock widget

--- src/ptz-controller.cpp
+++ src/ptz-controller.cpp
@@ -12,27 +12,15 @@
 #include <thread>
 #include <regex>
 #include <Processing.NDI.Lib.h>
-#include <QTimer>
+#include <mutex>
+#include <QMetaObject>
+#include <QThread>
+#include <QObject>
 
-typedef struct {
-	pthread_t ptz_thread;
-	bool running;
-	short x_delta;
-	short y_delta;
-	short x_start;
-	short y_start;
-	short x_move;
-	short y_move;
-	short zoom;
-	float pixels_to_pan;
-	float pixels_to_tilt;
-	visca_tuple_t pt_start;
-	bool mouse_down;
-	bool drag_start;
-	QWidget *dialog;
-} ptz_controller_t;
-
-static ptz_controller_t *_context;
+static std::mutex _imageMutex;
+static unsigned char *_imageData;
+static int _imageWidth;
+static int _imageHeight;
 
 // Function to convert a single UYVY pixel pair to two RGB pixels
 void UYVYtoRGB(int U, int Y1, int V, int Y2, QRgb &rgb1, QRgb &rgb2)
@@ -56,16 +44,13 @@ void UYVYtoRGB(int U, int Y1, int V, int Y2, QRgb &rgb1, QRgb &rgb2)
 }
 
 // Main conversion function
-QImage convertUYVYtoRGB32(const unsigned char *uyvyData, int width, int height)
+void convertUYVYtoRGB32(const unsigned char *uyvyData,
+					     int width, int height, unsigned char *rgbaData)
 {
-	
-    QImage image(width, height, QImage::Format_RGB32);
-	
 	for (int y = 0; y < height; ++y) {
 		for (int x = 0; x < width; x += 2) {
-			int index =
-				(y * width + x) *
-				2; // 2 bytes per pixel, but U and V are shared between two pixels
+			int index = (y * width + x) *
+				    2; 
 			int U = uyvyData[index];
 			int Y1 = uyvyData[index + 1];
 			int V = uyvyData[index + 2];
@@ -74,54 +59,32 @@ QImage convertUYVYtoRGB32(const unsigned char *uyvyData, int width, int height)
 			QRgb rgb1, rgb2;
 			UYVYtoRGB(U, Y1, V, Y2, rgb1, rgb2);
 
-			image.setPixel(x, y, rgb1);
-			image.setPixel(x + 1, y, rgb2);
+			int r1Index = (y * width + x) * 4;
+			unsigned int *r1ptr = (unsigned int *)&rgbaData[r1Index];
+			*r1ptr = rgb1;
+
+			if (x + 1 < width) { // Check to avoid buffer overflow
+				unsigned int *r2ptr =
+					(unsigned int *)&rgbaData[r1Index+4];
+				*r2ptr = rgb2;
+			}
 		}
 	}
-
-	return image;
 }
-class InteractiveCanvas : public QWidget {
-public:
-	explicit InteractiveCanvas(QWidget *parent = nullptr) : QWidget(parent)
+
+InteractiveCanvas::InteractiveCanvas(QWidget *parent)
+	: QWidget(parent)
 	{
 		setAttribute(
 			Qt::WA_StaticContents); // Indicates the widget content is static
-		// Setup the timer
-						
-	    QTimer *timer = new QTimer(this);
-	    connect(timer, &QTimer::timeout, this,
-		    &InteractiveCanvas::updateImage);
-	    timer->start(100); // 100 milliseconds = 0.1 seconds
-		
 	}
-	void updateImage()
+
+	void InteractiveCanvas::updateImage(int dummy)
 	{
-		NDIlib_video_frame_v2_t video_frame;
-		// Assume 'ndiReceiver' is your NDI receiver instance
-		NDIlib_recv_instance_t recv = ptz_preview_get_recv();
-		if (ptz_preview_get_ndilib()->recv_capture_v2(
-			    recv, &video_frame, nullptr, nullptr, 1000) ==
-		    NDIlib_frame_type_video) {
-			// Convert NDI frame to QImage
-			// QImage tempImage(video_frame.p_data, video_frame.xres,
-			//	     video_frame.yres, QImage::Format_RGB32);
-			QImage tempImage = convertUYVYtoRGB32(
-				video_frame.p_data, video_frame.xres,
-				video_frame.yres);
-			image = tempImage.copy(); // Copy if needed, depending on your use case
-
-			// Free the frame
-			ptz_preview_get_ndilib()->recv_free_video_v2(
-				recv, &video_frame);
-
-			// Request update
-			update();
-		}
+		update();
 	}
 
-protected:
-	void mousePressEvent(QMouseEvent *event) override
+	void InteractiveCanvas::mousePressEvent(QMouseEvent *event) 
 	{
 		if (event->button() == Qt::LeftButton) {
 			lastPoint = event->pos();
@@ -129,13 +92,13 @@ protected:
 		}
 	}
 
-	void mouseMoveEvent(QMouseEvent *event) override
+	void InteractiveCanvas::mouseMoveEvent(QMouseEvent *event)
 	{
 		if ((event->buttons() & Qt::LeftButton) && drawing)
 			drawLineTo(event->pos());
 	}
 
-	void mouseReleaseEvent(QMouseEvent *event) override
+	void InteractiveCanvas::mouseReleaseEvent(QMouseEvent *event)
 	{
 		if (event->button() == Qt::LeftButton && drawing) {
 			drawLineTo(event->pos());
@@ -143,27 +106,31 @@ protected:
 		}
 	}
 
-	void paintEvent(QPaintEvent *event) override
+	void InteractiveCanvas::paintEvent(QPaintEvent *event)
 	{
 		QPainter painter(this);
-		QSize scaledSize =
-			image.size().scaled(this->size(), Qt::KeepAspectRatio);
+		{
+			std::lock_guard<std::mutex> lock(_imageMutex);
+			if (_imageData != nullptr) {
+				QImage localCanvas = QImage(
+					_imageData, _imageWidth, _imageHeight,
+					QImage::Format_RGB32);
+				QSize scaledSize = localCanvas.size().scaled(
+					this->size(), Qt::KeepAspectRatio);
 
-		// Calculate the top left position to center the image
-		QPoint topLeft((this->width() - scaledSize.width()) / 2,
-			       (this->height() - scaledSize.height()) / 2);
-
-		// Draw the scaled image
-		painter.drawImage(QRect(topLeft, scaledSize), image);
+				// Calculate the top left position to center the image
+				QPoint topLeft(
+					(this->width() - scaledSize.width()) /
+						2,
+					(this->height() - scaledSize.height()) /
+						2);
+				painter.drawImage(QRect(topLeft, scaledSize),
+						  localCanvas);
+			}
+		}
 	}
 
-private:
-	bool drawing = false;
-	QImage canvas;
-	QPoint lastPoint;
-	QImage image;
-
-	void drawLineTo(const QPoint &endPoint)
+	void InteractiveCanvas::drawLineTo(const QPoint &endPoint)
 	{
 		QPainter painter(&canvas);
 		painter.setPen(QPen(Qt::black, 3, Qt::SolidLine, Qt::RoundCap,
@@ -177,7 +144,7 @@ private:
 		lastPoint = endPoint;
 	}
 
-	void resizeEvent(QResizeEvent *event) override
+	void InteractiveCanvas::resizeEvent(QResizeEvent *event)
 	{
 		if (width() > canvas.width() || height() > canvas.height()) {
 			int newWidth = qMax(width() + 128, canvas.width());
@@ -188,7 +155,7 @@ private:
 		QWidget::resizeEvent(event);
 	}
 
-	void resizeCanvas(const QSize &newSize)
+	void InteractiveCanvas::resizeCanvas(const QSize &newSize)
 	{
 		if (canvas.size() == newSize)
 			return;
@@ -199,8 +166,9 @@ private:
 		painter.drawImage(QPoint(0, 0), canvas);
 		canvas = newCanvas;
 	}
-};
+
 class PTZControllerWidget : public QWidget {
+
 public:
 	PTZControllerWidget()
 	{
@@ -209,7 +177,7 @@ public:
 		QVBoxLayout *vbox = new QVBoxLayout(this);
 		vbox->addWidget(label);
 
-		InteractiveCanvas *canvas = new InteractiveCanvas(this);
+		canvas = new InteractiveCanvas(this);
 		canvas->setSizePolicy(QSizePolicy::Expanding,
 				      QSizePolicy::Expanding);
 		vbox->addWidget(canvas);
@@ -229,13 +197,33 @@ protected:
 		label->setText(ptz_preview_get_ndiname().c_str());
 		//obs_source_t *source = ptz_preview_get_source();
 		//obs_data_t *settings = obs_source_get_settings(source);
-		//canvas->updateImage();
+		if (canvas) canvas->update();
 		//obs_data_release(settings);
 	};
 
 private:
 	QLabel *label;
 };
+typedef struct {
+
+	bool running;
+	short x_delta;
+	short y_delta;
+	short x_start;
+	short y_start;
+	short x_move;
+	short y_move;
+	short zoom;
+	float pixels_to_pan;
+	float pixels_to_tilt;
+	visca_tuple_t pt_start;
+	bool mouse_down;
+	bool drag_start;
+	PTZControllerWidget *dialog;
+} ptz_controller_t;
+
+static ptz_controller_t *_context;
+
 void ptz_controller_update()
 {
 	if (_context) _context->dialog->update();
@@ -304,78 +292,144 @@ void ptz_controller_focus(ptz_controller_t *s, bool focus) {
 		     s->pixels_to_tilt);
 	}
 };
-void *ptz_controller_thread(void *data)
-{
-    auto s = (ptz_controller_t *)data;
 
-    while (s->running) {
-	    if (ptz_preview_get_recv()) {
-		    if (s->y_delta != 0) {
-			    int newZoom = std::clamp(s->zoom + (s->y_delta * 4),
-						     0, 16384);
-			    blog(LOG_INFO,
-				 "[obs-ndi] ptz_controller zooming [%d] %d %d",
-				 s->y_delta, s->zoom, newZoom);
+class PTZControllerThread : public QThread {
+public:
+	PTZControllerThread() {}
 
-			    s->zoom = newZoom;			    
-				auto err = ptz_preview_get_visca_connection()
-					       .setZoomLevel(newZoom);
+protected:
+	void run() override
+	{	
+		auto s = _context;
+		blog(LOG_INFO, "[obs-ndi] ptz_controller_thread_run");
+		while (s->running) {
+			if (ptz_preview_get_recv()) {
+				if (s->y_delta != 0) {
+					int newZoom = std::clamp(
+						s->zoom + (s->y_delta * 4), 0,
+						16384);
+					blog(LOG_INFO,
+					     "[obs-ndi] ptz_controller zooming [%d] %d %d",
+					     s->y_delta, s->zoom, newZoom);
 
-			    s->y_delta = 0;
-		    }
-		    if (s->drag_start) {
-			    auto err =
-				    ptz_preview_get_visca_connection().getPanTilt(
-					    s->pt_start);
-			    auto errz =
-				    ptz_preview_get_visca_connection().getZoomLevel(
-					    s->zoom);
-			    s->pixels_to_pan = ptz_pixels_to_pan_ratio(s->zoom);
-			    s->pixels_to_tilt =
-				    ptz_pixels_to_tilt_ratio(s->zoom);
-			    blog(LOG_INFO,
-				 "[obs-ndi] ptz_controller_mouse_click start drag err=%d, errz=%d, xy[%d,%d] pt[%d,%d]",
-				 err, errz, s->x_start, s->y_start, s->pt_start.value1,
-				 s->pt_start.value2);
-			    s->drag_start = false;
-		    }
-		    if (s->mouse_down) {
-			    int dx = s->x_start - s->x_move;
-			    int dy = s->y_start - s->y_move;
+					s->zoom = newZoom;
+					auto err =
+						ptz_preview_get_visca_connection()
+							.setZoomLevel(newZoom);
 
-			    visca_tuple_t dest = {
-				    s->pt_start.value1 +
-					    (int)((float)dx * s->pixels_to_pan),
-				    s->pt_start.value2 +
-					    (int)((float)dy *
-						  s->pixels_to_tilt)};
-			    auto err = ptz_preview_get_visca_connection()
-					.setAbsolutePanTilt(dest);
-			    blog(LOG_INFO,
-				 "[obs-ndi] ptz_controller_mouse_move xy[%d,%d] pt[%d,%d] px[%6.4f,%6.4f]",
-				 s->x_start, s->y_start, dest.value1,
-				 dest.value2, s->pixels_to_pan, s->pixels_to_tilt);
-		    }
-	    }
+					s->y_delta = 0;
+				}
+				if (s->drag_start) {
+					auto err =
+						ptz_preview_get_visca_connection()
+							.getPanTilt(
+								s->pt_start);
+					auto errz =
+						ptz_preview_get_visca_connection()
+							.getZoomLevel(s->zoom);
+					s->pixels_to_pan =
+						ptz_pixels_to_pan_ratio(
+							s->zoom);
+					s->pixels_to_tilt =
+						ptz_pixels_to_tilt_ratio(
+							s->zoom);
+					blog(LOG_INFO,
+					     "[obs-ndi] ptz_controller_mouse_click start drag err=%d, errz=%d, xy[%d,%d] pt[%d,%d]",
+					     err, errz, s->x_start, s->y_start,
+					     s->pt_start.value1,
+					     s->pt_start.value2);
+					s->drag_start = false;
+				}
+				if (s->mouse_down) {
+					int dx = s->x_start - s->x_move;
+					int dy = s->y_start - s->y_move;
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(50));
-    };
-    return s;
+					visca_tuple_t dest = {
+						s->pt_start.value1 +
+							(int)((float)dx *
+							      s->pixels_to_pan),
+						s->pt_start.value2 +
+							(int)((float)dy *
+							      s->pixels_to_tilt)};
+					auto err =
+						ptz_preview_get_visca_connection()
+							.setAbsolutePanTilt(
+								dest);
+					blog(LOG_INFO,
+					     "[obs-ndi] ptz_controller_mouse_move xy[%d,%d] pt[%d,%d] px[%6.4f,%6.4f]",
+					     s->x_start, s->y_start,
+					     dest.value1, dest.value2,
+					     s->pixels_to_pan,
+					     s->pixels_to_tilt);
+				}
+
+				NDIlib_video_frame_v2_t video_frame;
+				// Assume 'ndiReceiver' is your NDI receiver instance
+				NDIlib_recv_instance_t recv =
+					ptz_preview_get_recv();
+				if (ptz_preview_get_ndilib()->recv_capture_v2(
+					    recv, &video_frame, nullptr,
+					    nullptr,
+					    1000) == NDIlib_frame_type_video) {
+					{
+						std::lock_guard<std::mutex> lock(
+							_imageMutex);
+						if (_imageData == nullptr) {
+							_imageData = (unsigned char
+									      *)
+								bzalloc(video_frame
+										.xres *
+									video_frame
+										.yres *
+									4);
+							_imageWidth =
+								video_frame.xres;
+							_imageHeight =
+								video_frame.yres;
+						}
+
+						convertUYVYtoRGB32(
+							video_frame.p_data,
+							video_frame.xres,
+							video_frame.yres,
+							_imageData);
+					}
+					// Free the frame
+					ptz_preview_get_ndilib()
+						->recv_free_video_v2(
+							recv, &video_frame);
+
+					bool success =
+						QMetaObject::invokeMethod(
+							s->dialog->canvas,
+							"updateImage",
+							Qt::QueuedConnection, Q_ARG(int,_imageWidth));
+				}
+			}
+			std::this_thread::sleep_for(
+				std::chrono::milliseconds(100));
+		};
+	}
+};
+
+static PTZControllerThread *ptzThread;
+
+
+void ptz_controller_thread_start(ptz_controller_t *s) {
+    s->running = true;
+    ptzThread = new PTZControllerThread(); // Assuming ptzThread is added to ptz_controller_t
+    ptzThread->start();
+    blog(LOG_INFO, "[obs-ndi] ptz_controller_thread_start");
 }
 
-void ptz_controller_thread_start(ptz_controller_t *s)
-{
-	s->running = true;
-	pthread_create(&s->ptz_thread, nullptr, ptz_controller_thread, s);
-	blog(LOG_INFO, "[obs-ndi] ptz_controller_thread_start");
-}
-
-void ptz_controller_thread_stop(ptz_controller_t *s)
-{
+void ptz_controller_thread_stop(ptz_controller_t *s) {
     if (s->running) {
         s->running = false;
-        pthread_join(s->ptz_thread, NULL);
+        ptzThread->quit();
+        ptzThread->wait();
+        delete ptzThread;
     }
+    blog(LOG_INFO, "[obs-ndi] ptz_controller_thread_stop");
     bfree(s);
 }
 
@@ -386,7 +440,7 @@ ptz_controller_init()
 
 	_context = (ptz_controller_t *)bzalloc(sizeof(ptz_controller_t));
     _context->dialog = new PTZControllerWidget();
-
+	_imageData = nullptr;
     _context->running = false;
 
     obs_frontend_add_dock_by_id(
--- src/ptz-controller.h
+++ src/ptz-controller.h
@@ -1,9 +1,28 @@
 #pragma once
 #include <Processing.NDI.Lib.h>
 #include "../lib/visca27/ViscaAPI.h"
+
 #include <pthread.h>
 #include <string>
 #include <qwidget.h>
-
+class InteractiveCanvas : public QWidget {
+	Q_OBJECT
+public:
+	explicit InteractiveCanvas(QWidget *parent = nullptr);
+public slots:
+	void updateImage(int dummy);
+protected:
+	void mousePressEvent(QMouseEvent *event) override;
+	void mouseMoveEvent(QMouseEvent *event) override;
+	void mouseReleaseEvent(QMouseEvent *event) override;
+	void paintEvent(QPaintEvent *event) override;
+private:
+	bool drawing = false;
+	QImage canvas;
+	QPoint lastPoint;
+	void drawLineTo(const QPoint &endPoint);
+	void resizeEvent(QResizeEvent *event) override;
+	void resizeCanvas(const QSize &newSize);
+};
 void ptz_controller_init();
 void ptz_controller_update();
